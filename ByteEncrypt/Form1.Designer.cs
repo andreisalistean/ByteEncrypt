476162256b0b4563bce14fe321158beef1df0350
vasil <vasil@DESKTOP-EHKCC4T>
2023-02-17 15:00:28 +0200

    Add project files.

---
+++ ByteEncrypt/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ByteEncrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPathMask = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.buttonTxtFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPathMask
+            // 
+            this.txtPathMask.Location = new System.Drawing.Point(202, 67);
+            this.txtPathMask.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPathMask.Name = "txtPathMask";
+            this.txtPathMask.Size = new System.Drawing.Size(264, 31);
+            this.txtPathMask.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(38, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mask file";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(501, 48);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(130, 50);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(202, 141);
+            this.txtText.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(264, 31);
+            this.txtText.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(38, 141);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Text file";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(500, 246);
+            this.btnEncrypt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(131, 59);
+            this.btnEncrypt.TabIndex = 5;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(29, 247);
+            this.btnDecrypt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(135, 58);
+            this.btnDecrypt.TabIndex = 6;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // buttonTxtFile
+            // 
+            this.buttonTxtFile.Location = new System.Drawing.Point(501, 123);
+            this.buttonTxtFile.Name = "buttonTxtFile";
+            this.buttonTxtFile.Size = new System.Drawing.Size(136, 50);
+            this.buttonTxtFile.TabIndex = 7;
+            this.buttonTxtFile.Text = "Browse";
+            this.buttonTxtFile.UseVisualStyleBackColor = true;
+            this.buttonTxtFile.Click += new System.EventHandler(this.buttonTxtFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 355);
+            this.Controls.Add(this.buttonTxtFile);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPathMask);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPathMask;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button buttonTxtFile;
+    }
+}
+
---
+++ ByteEncrypt/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+
+namespace ByteEncrypt
+{
+    public partial class Form1 : Form
+    {
+        private string text,terminator;
+        public Form1()
+        {
+            InitializeComponent();
+            text = "";
+            terminator = "$finish$";
+        }
+
+        bool check_terminator()
+        {
+            if (text.Contains(terminator))
+                return true;
+            else return false;
+        }
+
+        
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Multiselect = false;
+            if (od.ShowDialog() == DialogResult.OK)
+            {
+                txtPathMask.Text = od.FileName;
+            }
+        }
+
+        
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if(!File.Exists(txtPathMask.Text))
+            {
+                MessageBox.Show("Fisierul imagine nu exista");
+                return;
+            }
+            if (!File.Exists(txtText.Text))
+            {
+                MessageBox.Show("Fisierul imagine nu exista");
+                return;
+            }
+
+            try
+            {
+                Bitmap img = new Bitmap(txtPathMask.Text.ToString());
+                
+                byte[] a = File.ReadAllBytes(txtText.Text.ToString());
+                byte[] b = Encoding.UTF8.GetBytes(terminator);
+                byte[] message = new byte[a.Length+b.Length];
+                Array.Copy(a, 0, message, 0, a.Length);
+                Array.Copy(b, 0, message, a.Length, b.Length);
+
+                byte masca = 0x03,rez,m2;
+
+                Color pix;
+                byte p_maskR,p_maskG,p_maskB;
+
+                int indice_message = 0;
+                short bit=0;
+            
+                for(int x=0;x<img.Width;++x)
+                {
+                    if (indice_message >= message.Length)
+                        break;
+                    for (int y=0;y<img.Height;++y)
+                    {
+                        if (indice_message >= message.Length)
+                            break;
+                        pix = img.GetPixel(x,y);
+
+                        p_maskR = pix.R;
+                        p_maskG = pix.G;
+                        p_maskB = pix.B;
+
+                        for(short c=0;c<3;++c)
+                        {
+                            if (indice_message >= message.Length)
+                                break;
+                            m2 = masca;
+                            m2=(byte)(m2 << bit);
+                            rez = (byte)(message[indice_message] & m2);
+                            rez = (byte)(rez >> bit);
+                            rez = (byte)(rez | 0xFC);
+                            if (c==0)//R
+                            {
+                                p_maskR = (byte)(p_maskR | 0x03);
+                                p_maskR = (byte)(p_maskR & rez);
+                            }
+                            else if(c==1)//G
+                            {
+                                p_maskG = (byte)(p_maskG | 0x03);
+                                p_maskG = (byte)(p_maskG & rez);
+                            }
+                            else if(c==2)//B
+                            {
+                                p_maskB = (byte)(p_maskB | 0x03);
+                                p_maskB = (byte)(p_maskB & rez);
+                            }
+                            bit += 2;
+                            if(bit==8)
+                            {
+                                bit = 0;
+                                ++indice_message;
+                            }
+                        }
+                        img.SetPixel(x, y, Color.FromArgb(p_maskR, p_maskG, p_maskB));
+                    }
+                }
+                String fileExt = Path.GetExtension(txtPathMask.Text);
+                SaveFileDialog sd = new SaveFileDialog();
+                sd.Filter = "Files (" + fileExt + ") | " + fileExt;
+                if (sd.ShowDialog() == DialogResult.OK)
+                {
+                    img.Save(sd.FileName);
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Fisierul nu s-a putut deschide"+ex.ToString()+ex.Source);
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            text = "";
+            if (!File.Exists(txtPathMask.Text))
+            {
+                MessageBox.Show("Fisierul nu exista");
+                return;
+            }
+            try
+            {
+                Bitmap img = new Bitmap(txtPathMask.Text);
+
+                byte masca = 0x03, carry=0x00,rez=0x00;
+
+                Color pix;
+
+                short bit = 0;
+                //char litera;
+
+                for(int x=0;x<img.Width;++x)
+                {
+                    if (check_terminator())
+                        break;
+                    for (int y=0;y<img.Height;++y)
+                    {
+                        if (check_terminator())
+                            break;
+                        pix = img.GetPixel(x, y);
+                        for(short c=0;c<3;++c)
+                        {
+                            if (check_terminator())
+                                break;
+                            if(c==0)//R
+                            {
+                                rez = (byte)(pix.R & masca);
+                            }
+                            else if(c==1)//G
+                            {
+                                rez = (byte)(pix.G & masca);
+                            }
+                            else if(c==2)//B
+                            {
+                                rez = (byte)(pix.B & masca);
+                            }
+                            rez = (byte)(rez << bit);
+                            carry = (byte)(carry | rez);
+                            bit += 2;
+                            if(bit==8)
+                            {
+                                bit = 0;
+                                text += (char)carry;
+                                carry = 0x00;
+                            }
+                        }
+                    }
+                }
+                SaveFileDialog sd = new SaveFileDialog();
+                sd.Filter = "Files ( txt ) | txt";
+                if (sd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(sd.FileName, text);
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Fisierul nu s-a putut deschide" + ex.ToString() + ex.Source);
+            }
+        }
+
+        private void buttonTxtFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Multiselect = false;
+            if (od.ShowDialog() == DialogResult.OK)
+            {
+                txtText.Text = od.FileName;
+            }
+        }
+    }
+}
